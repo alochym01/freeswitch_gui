a473e7d13c57cd2ef14fd3c11c4037cda1f4651b
alochym01 <wowebookpro@gmail.com>
2013-05-21 13:49:33 +0700

    add home page for freeswitch

--- freeswitch_gui/urls.py
+++ freeswitch_gui/urls.py
@@ -14,4 +14,6 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', 'freeswitch_gui.views.home', name='home'),
+    url(r'^subscriber/', include('subscriber.urls')),
 )
